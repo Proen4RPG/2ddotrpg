33288e2d71bb0c1e3188056d75e9fd86e84d3293
Keita <kitswd11@gmail.com>
2017-08-22 21:57:13 +0900

    Save/Load 機能の追加

---
+++ KTest/Assets/Scripts/SaveTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SaveTest : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        GV.save();
+        GV.load();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
