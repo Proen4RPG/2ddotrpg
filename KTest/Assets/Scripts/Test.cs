8d87feae965a8a64529a3d79b53cb9292434ef27
Keita <kitswd11@gmail.com>
2017-08-22 23:43:46 +0900

    UI 関係の機能を追加 だいぶ問題あり

---
+++ KTest/Assets/Scripts/Test.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField]
+    MyMenuFrame menu;
+
+    // Use this for initialization
+    void Start()
+    {
+        GV.save();
+        GV.load();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) && ButtonManager.isAllClosed()) {
+            ButtonManager.openWindow(menu);
+        }
+        if (Input.GetButtonDown("Cancel")) {
+            ButtonManager.cancel();
+        }
+    }
+}
