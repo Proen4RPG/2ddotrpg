c9186088bca3f4b812ba8e7472039ab90a24cd06
hironari ushiyama <lovemika9528202605@yahoo.co.jp>
2017-09-02 14:54:09 +0900

    仮テスト用Bloom Spriteの作成及びアニメーション動作処理の追加 ※敵を選択したときの感じを出すための仮実験です

---
+++ testPlayerSprite/Purge.bat
@@ -0,0 +1,17 @@
+rem 参考サイト http://d.hatena.ne.jp/nakamura001/20090203/1233652705
+cd /d %~dp0
+
+rem del /s *.log
+
+rem 参考サイト http://tooljp.com/bat_qa/4B0ADEC0831C7D5F49257E4F000E1F58.html
+rem 現在のディレクトリ配下の指定フォルダ削除するコマンド
+rem コマンドプロンプトとバッチファイル内での記述の仕方は違うので注意する
+
+rem インポートした Assets を保管するローカルのキャッシュ
+for /R %%d in (Library) do rmdir /S /Q "%%d"
+rem Build した時に一時的にできるファイルを保管する場所
+for /R %%d in (Temp) do rmdir /S /Q "%%d"
+
+for /R %%d in (Obj) do rmdir /S /Q "%%d"
+
+for /R %%d in (.vs) do rmdir /S /Q "%%d"
