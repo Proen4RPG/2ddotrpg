c7d814a4b761aff2add10775ea6891c40c66ed40
Terada <shin,yakishi222@gmail.com>
2017-10-17 10:34:21 +0900

    metadata諸々

---
+++ testPlayerSprite/Assets/Script/UI/SPAllocation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SPAllocation : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+		
+	}
+
+    void Initialize() {
+
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+	}
+}
